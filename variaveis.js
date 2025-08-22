134d4b0a230a96bef59ab9efbbe46aa55a54d659
Gabriel Moreno Magalhães <823117822@ulife.com.br>
2025-08-22 11:13:17 -0300

    Conhecendo variaveis em JavaScript

---
+++ variaveis.js
@@ -0,0 +1,38 @@
+var nome = "buno"
+const altura = 173
+let idade = 22
+console.log("Meu nome é " + nome + ", minha altura é " + altura + " e minha idade é " + idade)
+
+let texto = "string"
+
+let numero = 10
+let numero2 = 10.5
+
+let boolean = true
+let boolean2 = false
+
+let nulo = null 
+let indefinido = undefined
+
+let objeto = {nome: "buno", idade: 22, altura: 173}
+
+let lista = ["uva","maçã","manana"]
+
+//Conversão de dados
+
+// String pra numero
+
+let StringParaNumero = Number("10")
+let StringParaNumero2 = parseInt("10.5") //converte para inteiro
+let StringParaNumero3 = parseFloat("10,5") //converte para float
+
+//numero para String
+
+let str = string(100)
+let str2 = (100).toString();
+
+//Boolean para número
+
+let BooleanParaNumero = Number(true);
+let BooleanParaNumero2 = Number(false);
+
