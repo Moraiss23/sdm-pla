16b53b330296a16e5b9791f3baa4d9eb44fb036c
Bruno Morais dos Santos <823122109@ulife.com.br>
2025-09-05 11:37:32 -0300

    monolitinho

--- monolito.js
+++ monolito.js
@@ -1,4 +1,30 @@
 const express = require("express")
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
+
+let usuarios = [];
+let pedidos = [];
+
+app.post("/usuarios", (req, res) =>{
+    const usuario = req.body
+    usuarios.push(usuario)
+    res.send({ message: "Usuario criado com sucesso!"})
+})
+
+app.post("/pedidos", (req, res) =>{
+
+    const pedido = req.body;
+    pedidos.push(pedido);
+    res.send({message: "Pedido criado com sucesso!"})
+
+})
+
+app.get("/dados", (req,res) => {
+    res.send( {usuarios, pedidos} )
+
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+})
